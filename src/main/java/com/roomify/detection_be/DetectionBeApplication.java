db29529c709551444b75d101df5156b954865322
DigiEx-HuuPhuc <nguyenhuuthuc0405@gmail.com>
2024-12-22 11:33:36 +0700

    First commit

---
+++ src/main/java/com/roomify/detection_be/DetectionBeApplication.java
@@ -0,0 +1,13 @@
+package com.roomify.detection_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectionBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetectionBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roomify/detection_be/DetectionBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomify.detection_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetectionBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
