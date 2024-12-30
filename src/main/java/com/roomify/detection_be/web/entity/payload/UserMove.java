8d5d85917fc6aeb3cb229bf753d1516652819d22
DigiEx-HuuPhuc <nguyenhuuthuc0405@gmail.com>
2024-12-30 11:41:26 +0700

    Clean code

--- src/main/java/com/roomify/detection_be/web/entity/payload/UserMove.java
+++ src/main/java/com/roomify/detection_be/web/entity/payload/UserMove.java
@@ -1,7 +1,13 @@
 package com.roomify.detection_be.web.entity.payload;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
 @Getter
 public class UserMove {
   private String username;
