5ad1475cf86d921128d09fd2586396a5a047eb7d
Rvt129 <baothuvoal@gmail.com>
2025-01-03 02:43:45 +0700

    RedisInit

--- src/main/java/com/roomify/detection_be/web/entity/User.java
+++ src/main/java/com/roomify/detection_be/web/entity/User.java
@@ -1,10 +1,7 @@
 package com.roomify.detection_be.web.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
