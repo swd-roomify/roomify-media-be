2fbd3b21866f696b3e4e265a144c49a414d39290
Hyudeque <quanghuy01062004@gmail.com>
2025-01-18 12:43:33 +0700

    Update RedisConfig.java

--- src/main/java/com/roomify/detection_be/config/RedisConfig.java
+++ src/main/java/com/roomify/detection_be/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.roomify.detection_be.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -11,10 +12,14 @@ import com.roomify.detection_be.web.entity.User;
 
 @Configuration
 public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
 
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
     @Bean
