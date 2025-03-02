2c33abc26d5027ff63faa69022b66ee330632343
DigiEx-HuuPhuc <phuc.nguyen@digiex.group>
2025-03-02 10:54:54 +0700

    Clean code

--- src/main/java/com/roomify/detection_be/repository/FriendShipRepository.java
+++ src/main/java/com/roomify/detection_be/repository/FriendShipRepository.java
@@ -2,10 +2,9 @@ package com.roomify.detection_be.repository;
 
 import com.roomify.detection_be.web.entities.Friendship;
 import com.roomify.detection_be.web.entities.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface FriendShipRepository extends JpaRepository<Friendship,String> {
-    Optional<Friendship> findByUser1AndUser2(Optional<User> user1, Optional<User> byId);
+public interface FriendShipRepository extends JpaRepository<Friendship, String> {
+  Optional<Friendship> findByUser1AndUser2(Optional<User> user1, Optional<User> byId);
 }
